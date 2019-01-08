ad487040b1889fe1a0ed7f2d99f9964fe93aa43b
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-01-08 23:36:49 +0500

    clarifai => backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,37 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "4a1dcea0b8ee4974b20ffefea7bed5cd"
+});
+
 const handleImage = (request, response, knex, userResponse) => {
-  const { id } = request.body;
+  const { id, input } = request.body;
+  let boxes = [];
   const returnedUser = {};
-  knex("users")
-    .where({ id: Number(id) })
-    .increment("entries", 1)
-    .returning("*")
-    .then(updatedUser => {
-      Object.assign(returnedUser, updatedUser[0]);
-      response.json(userResponse(returnedUser));
+  app.models
+    .predict("a403429f2ddf4b49b307e318f00e528b", input)
+    .then(res => {
+      if (res.outputs[0].data.regions) {
+        boxes = res.outputs[0].data.regions.map(
+          box => box.region_info.bounding_box
+        );
+        console.log(boxes);
+      } else {
+        throw new Error("Face not found");
+      }
     })
-    .catch(() => response.status(400).json({ status: "Error updating entry" }));
+    .then(() =>
+      knex("users")
+        .where({ id: Number(id) })
+        .increment("entries", 1)
+        .returning("*")
+        .then(updatedUser => {
+          Object.assign(returnedUser, updatedUser[0]);
+          console.log(userResponse(returnedUser), boxes);
+          response.json({ user: userResponse(returnedUser), boxes });
+        })
+    )
+    .catch(error => response.json({ status: error, boxes }));
 };
 
 module.exports = {
