fc434a843b5dc975ebe1828e9b25b03839966ef6
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-01-10 14:21:37 +0500

    $Port for Heroku

--- server.js
+++ server.js
@@ -48,4 +48,6 @@ app.put("/image", (request, response) =>
   handleImage(request, response, knex, userResponse)
 );
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
