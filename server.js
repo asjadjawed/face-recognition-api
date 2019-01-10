cf288735d58d4033672f767037b0da889b20af86
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-01-10 13:22:00 +0500

    Pre-deploymnet

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const knex = require("knex")({
   connection: {
     host: "127.0.0.1",
     user: "postgres",
-    password: "sqpants77",
+    password: "hunter2",
     database: "face-recognition"
   }
 });
